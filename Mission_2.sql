9be7574f860e93d369d89977e9f1701a0421b2b5
Evank2023 <148534119+Evank2023@users.noreply.github.com>
2024-07-05 13:05:01 +0200

    Add files via upload

---
+++ Mission_2.sql
@@ -0,0 +1,211 @@
+UPDATE Sales.InvoiceLines
+
+SET UnitPrice = UnitPrice + 20
+
+WHERE InvoiceId = 69627 AND InvoiceLineID = 225394 ;
+
+
+
+SELECT
+
+t.CustomerID,
+
+t.CustomerName,
+
+SUM(t.TotalNBOrders) as TotalNBOrders,
+
+SUM(t.TotalNBInvoices) as TotalNBInvoices,
+
+SUM(t.OrdersTotalValue) as OrdersTotalValue,
+
+SUM(t.InvoicesTotalValue) as InvoicesTotalValue,
+
+ABS(SUM(OrdersTotalValue) - SUM(InvoicesTotalValue)) as AbsoluteValueDifference
+
+FROM
+
+(
+
+(
+
+SELECT
+
+Cu.CustomerID,
+
+Cu.CustomerName,
+
+COALESCE(COUNT(DISTINCT O.OrderId),0) AS TotalNBOrders,
+
+0 AS TotalNBInvoices,
+
+COALESCE(SUM(OL.Quantity * OL.UnitPrice),0) as OrdersTotalValue,
+
+0 as InvoicesTotalValue,
+
+0 as AbsoluteValueDifference
+
+
+
+
+
+FROM Sales.Customers as Cu,
+
+Sales.Orders as O,
+
+Sales.OrderLines as OL
+
+
+
+WHERE
+
+O.CustomerId = Cu.CustomerID
+
+AND OL.OrderID = O.OrderID
+
+AND EXISTS
+
+(
+
+SELECT *
+
+FROM Sales.Invoices AS IP1
+
+WHERE
+
+IP1.OrderID = O.OrderID
+
+
+
+)
+
+GROUP BY
+
+Cu.CustomerID,
+
+Cu.CustomerName
+
+)
+
+UNION 
+
+(
+
+SELECT
+
+Cu.CustomerID,
+
+Cu.CustomerName,
+
+0 AS TotalNBOrders,
+
+COALESCE(COUNT(DISTINCT I.InvoiceID),0) AS TotalNBInvoices,
+
+0 as OrdersTotalValue,
+
+COALESCE(SUM(IL.Quantity * IL.UnitPrice),0) as InvoicesTotalValue,
+
+0 as AbsoluteValueDifference
+
+
+
+
+
+FROM Sales.Customers as Cu,
+
+Sales.Invoices as I,
+
+Sales.InvoiceLines as IL
+
+
+
+WHERE
+
+I.CustomerId = Cu.CustomerID
+
+AND IL.InvoiceID = I.InvoiceID
+
+AND EXISTS
+
+(
+
+SELECT *
+
+FROM Sales.Orders AS OP2
+
+WHERE
+
+OP2.OrderID = I.OrderID
+
+
+
+)
+
+
+
+GROUP BY
+
+Cu.CustomerID,
+
+Cu.CustomerName
+
+)
+
+) as t
+
+
+
+GROUP BY
+
+CustomerID,
+
+CustomerName
+
+
+
+ORDER BY
+
+AbsoluteValueDifference DESC,
+
+TotalNBOrders,
+
+CustomerName;
+
+
+
+-- Q2 ALTERNATIVE
+
+
+
+USE WideWorldImporters;
+
+
+
+UPDATE Sales.InvoiceLines
+
+SET UnitPrice = UnitPrice + 20
+
+WHERE InvoiceId = 69627 AND InvoiceLineID = 225394
+
+
+
+SELECT Cu.CustomerID, Cu.CustomerName, SUM(IL.Quantity * IL.UnitPrice) as InvoiceTotal
+
+FROM Sales.Customers as Cu,
+
+Sales.Invoices as I,
+
+Sales.InvoiceLines as IL
+
+WHERE
+
+Cu.CustomerId = 1060
+
+AND I.InvoiceID = 69627
+
+AND I.CustomerID = Cu.CustomerId
+
+AND IL.InvoiceID = I.InvoiceID
+
+GROUP BY
+
+Cu.CustomerID, Cu.CustomerName;
